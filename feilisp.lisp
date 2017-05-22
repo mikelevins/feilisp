08b06eaa097f258e7cd40ca8f8e2d40ea641c97c
mikel evins <mevins@me.com>
2017-05-22 16:52:16 -0500

    tinkering with phonetics/morphology tools

--- feilisp.lisp
+++ feilisp.lisp
@@ -1,172 +1,297 @@
-;;;; feilisp.lisp
-
 (in-package #:feilisp)
 
 (defparameter $consonants
   '(m n g f d s x c h r l i u))
 
-(defmethod any ((s sequence))
-  (elt s (random (length s))))
-
-(defun digraphs ()
-  (mapcar (lambda (pair)
-            (intern (concatenate 'string
-                                 (symbol-name (first pair))
-                                 (symbol-name (second pair)))
-                    :feilisp))
-          (remove-duplicates
-           (reduce #'append
-                   (loop for left in $consonants
-                      collect (loop for right in $consonants
-                                 collect (list left right)))))))
-
-(defparameter $digraphs (digraphs))
-
-(defmethod list-difference ((l null) r) nil)
-(defmethod list-difference (l (r null)) l)
-(defmethod list-difference ((l null)(r null)) nil)
-
-(defmethod list-difference ((l cons)(r cons))
-  (let ((result l))
-    (loop for thing in r
-       do (setf result (remove thing result :test #'equal)))
-    result))
-
-(defparameter $initials
-  '(m n g f d s x c h r l i u
-    mn mi mu
-    ni nu
-    gi gu
-    fh fr fl fi fu
-    dh dr dl di du
-    sm sn sf sd sc sh sr sl si su
-    xm xn xf xd xc xh xr xl xi xu
-    cm cn ch cr cl ci cu
-    hr hl hi hu
-    rh ri ru
-    lh li lu))
-
-(defparameter $finals
-  '(m n g f d s x c h r l i u
-    mn mf md ms mx mc
-    nf nd ns nx nc
-    gs gx gc
-    fs fx
-    ds dx
-    cs
-    hm hn hg hf hd hs hx hc hr hl
-    rm rn rg rf rd rs rx rc rl
-    lm ln lg lf ld ls lx lc lh
-    im in ig if id is ix ic ir il
-    um un ug uf ud us ux uc))
-
-
-(defparameter $initials-in-use
-  '(c cl cr cu d di dl dr du f fi fl fr fu g h hl hr hu i l li lu m mi mu n ni nu r s sc sd sf sl sm sn su u x xl xr xu))
-
-(defparameter $finals-in-use
-  '(c cs d f g gc h i ic id il in ir is ix l ld ls m n nd ns nx r rc
-    rd rf rs s u ud uf um un x))
-
-(defparameter $medial-clusters
-  (remove-duplicates
-   (reduce #'append
-           (loop for f in $finals-in-use
-              collect (loop for i in $initials-in-use
-                         collect (list f i))))
-   :test #'equal))
-
 (defparameter $vowels
   '(i u o e a ii uu oo ee aa))
 
+(defun permute-parts (left-parts right-parts)
+  (mapcar (lambda (comb)
+            (intern (concatenate 'string
+                                 (symbol-name (first comb))
+                                 (symbol-name (second comb)))
+                    :feilisp))
+          (remove-duplicates
+           (reduce #'append
+                   (loop for left in left-parts
+                      collect (loop for right in right-parts
+                                 collect (list left right))))
+           :test #'eq)))
+
 (defparameter $diphthongs
-  '(iu uo oe ea ai ia ae eo ou ui))
+  (let* ((vwls (remove-if (lambda (v)(> (length (symbol-name v)) 1))
+                          $vowels))
+         (dips (permute-parts vwls vwls)))
+    (remove-if (lambda (dip)
+                 (let ((s (symbol-name dip)))
+                   (equal (elt s 0)
+                          (elt s 1))))
+               dips)))
+
+(defparameter $base-syllables
+  '((c l)
+    (c ls)
+    (c n)
+    (c rs)
+    (c s)
+    (c t)
+    (cl ir)
+    (cl s)
+    (cl t)
+    (cl w)
+    (cr ic)
+    (cr t)
+    (cr wt)
+    (cr yt)
+    (cw ir)
+    (cw n)
+    (f c)
+    (f g)
+    (f h)
+    (f l)
+    (f m)
+    (f n)
+    (f ns)
+    (f nx)
+    (f r)
+    (f s)
+    (f t)
+    (f wm)
+    (f x)
+    (f yr)
+    (fl c)
+    (fl n)
+    (fl w)
+    (fl yt)
+    (fr cs)
+    (fr l)
+    (fr m)
+    (fr n)
+    (fr t)
+    (fw r)
+    (fw t)
+    (fw yt)
+    (fy t)
+    (g l)
+    (g n)
+    (g wf)
+    (g wm)
+    (g wn)
+    (g wt)
+    (g yr)
+    (g yt)
+    (h l)
+    (h n)
+    (h r)
+    (h t)
+    (h x)
+    (h yr)
+    (hl t)
+    (hl wn)
+    (hr l)
+    (hr t)
+    (hr yt)
+    (hw l)
+    (hw yl)
+    (l c)
+    (l f)
+    (l g)
+    (l m)
+    (l n)
+    (l r)
+    (l t)
+    (l wm)
+    (l wn)
+    (l y)
+    (l yl)
+    (l yn)
+    (l yr)
+    (l yt)
+    (l yt)
+    (lw c)
+    (lw it)
+    (lw l)
+    (lw n)
+    (lw r)
+    (lw t)
+    (lw y)
+    (lw yx)
+    (ly c)
+    (ly l)
+    (ly n)
+    (ly r)
+    (ly t)
+    (ly w)
+    (ly wn)
+    (ly wt)
+    (m c)
+    (m g)
+    (m l)
+    (m n)
+    (m nt)
+    (m nx)
+    (m r)
+    (m rs)
+    (m s)
+    (m t)
+    (m w)
+    (m wm)
+    (m wt)
+    (m yn)
+    (m yr)
+    (mw c)
+    (mw g)
+    (mw l)
+    (mw n)
+    (mw r)
+    (mw t)
+    (mw yl)
+    (mw yr)
+    (mw ys)
+    (mw yt)
+    (my l)
+    (my s)
+    (n c)
+    (n g)
+    (n m)
+    (n r)
+    (n s)
+    (n t)
+    (n wm)
+    (n wn)
+    (n wt)
+    (n x)
+    (n y)
+    (nw g)
+    (nw l)
+    (nw m)
+    (nw r)
+    (ny c)
+    (ny m)
+    (ny n)
+    (ny r)
+    (r c)
+    (r cs)
+    (r gc)
+    (r l)
+    (r m)
+    (r n)
+    (r t)
+    (r w)
+    (r wn)
+    (r x)
+    (r y)
+    (r yl)
+    (r yn)
+    (r ys)
+    (r yx)
+    (s c)
+    (s f)
+    (s g)
+    (s l)
+    (s ls)
+    (s n)
+    (s r)
+    (s t)
+    (s w)
+    (s wm)
+    (sc n)
+    (sc y)
+    (sc yl)
+    (sf g)
+    (sf r)
+    (sl c)
+    (sl yl)
+    (sm g)
+    (sm l)
+    (sm n)
+    (sm w)
+    (sn m)
+    (sn w)
+    (st l)
+    (sw c)
+    (sw l)
+    (sw r)
+    (t c)
+    (t g)
+    (t l)
+    (t m)
+    (t n)
+    (t r)
+    (t rc)
+    (t wm)
+    (t wt)
+    (t y)
+    (tl c)
+    (tl r)
+    (tr c)
+    (tr g)
+    (tr l)
+    (tr m)
+    (tr n)
+    (tr w)
+    (tw c)
+    (tw g)
+    (tw r)
+    (tw t)
+    (tw yl)
+    (ty m)
+    (ty n)
+    (w l)
+    (w lt)
+    (w n)
+    (w ns)
+    (w nt)
+    (w r)
+    (w rt)
+    (w t)
+    (w yl)
+    (w yr)
+    (w ys)
+    (x c)
+    (x f)
+    (x h)
+    (x ir)
+    (x l)
+    (x n)
+    (x rc)
+    (x rf)
+    (x s)
+    (x t)
+    (x wn)
+    (x yc)
+    (x yl)
+    (x yt)
+    (xl yc)
+    (xr yc)
+    (xw yl)
+    (y f)
+    (y l)
+    (y n)
+    (y r)
+    (y s)
+    (y t)
+    (y wf)))
+
+(defparameter $base-initials
+  (delete-duplicates (mapcar #'first $base-syllables)))
+
+(defparameter $base-finals
+  (delete-duplicates (mapcar #'second $base-syllables)))
 
 (defparameter $cv-particles
   (remove-duplicates
-   (reduce #'append
-           (loop for left in $initials
-              collect (loop for right in (append $vowels $diphthongs)
-                         collect (list left right))))))
+   (append
+    (permute-parts $base-initials
+                   $vowels)
+    (permute-parts $base-initials
+                   $diphthongs))))
 
 (defparameter $vc-particles
   (remove-duplicates
-   (reduce #'append
-           (loop for left in (append $vowels $diphthongs)
-              collect (loop for right in $finals
-                         collect (list left right))))))
-
-(defparameter $cvc-syllables
-  (remove-duplicates
-   (reduce #'append
-           (loop for left in $initials
-              collect (loop for right in $finals
-                         collect (list left right))))))
-
-(defun print-syllable (syl)
-  (let ((*print-case* :downcase))
-    (format t "~A-~A" (first syl)(second syl))))
-
-(defun print-syllables (syls)
-  (let ((*print-case* :downcase))
-    (format t "~%")
-    (loop for s in syls
-       do (format t "~A-~A~%" (first s)(second s)))))
-
-(defun any-n-syllables (n)
-  (let ((result nil))
-    (loop while (> n (length result))
-       do (pushnew (any $syllables) result))
-    result))
-
-(defmethod save-cvc-syllables ((path pathname))
-  (let ((*print-case* :downcase))
-    (with-open-file (out path :direction :output)
-      (loop for syllable in $cvc-syllables
-         do (format out "~A-~A~%"
-                    (first syllable)
-                    (second syllable))))))
-
-(defmethod save-cvc-syllables ((path string))
-  (save-cvc-syllables (pathname path)))
-
-;;; (save-cvc-syllables "/Users/mikel/Desktop/fey-syllables.txt")
-
-(defmethod save-base-vocabulary ((path pathname))
-  (let* ((heads (remove-duplicates (mapcar #'first $syllables))))
-    (let ((*print-case* :downcase))
-      (with-open-file (out path :direction :output)
-        (let* ((syllables-table (loop for head in heads
-                                   collect (cons head
-                                                 (remove-if-not (lambda (x)(eql head (car x)))
-                                                                $syllables))))
-               (col-count (length syllables-table))
-               (row-count (apply #'max (mapcar #'length syllables-table)))
-               (syllables-array (make-array (list col-count row-count) :initial-contents syllables-table)))
-          (loop for y from 0 below row-count
-             do (progn
-                  (loop for x from 0 below col-count
-                     do (let ((it (aref syllables-array x y)))
-                          (typecase it
-                            (symbol (format out "~A, " it))
-                            (cons (format out "~A-~A, " (first it)(second it)))
-                            (t (error "Invalid element: ~S" it)))))
-                  (format out "~%"))))))))
-
-;;; (save-base-vocabulary "/Users/mikel/Desktop/fey-base-vocab.txt")
-
-(defmethod save-base-vocabulary ((path string))
-  (save-base-vocabulary (pathname path)))
-
-(defmethod save-medial-clusters ((path pathname))
-  (let ((*print-case* :downcase))
-    (with-open-file (out path :direction :output)
-      (loop for cluster in $medial-clusters
-         do (format out "~A,~A,~%"
-                    (first cluster)
-                    (second cluster))))))
-
-(defmethod save-medial-clusters ((path string))
-  (save-medial-clusters (pathname path)))
-
-;;; (save-medial-clusters "/Users/mikel/Desktop/fey-medials.csv")
+   (append
+    (permute-parts $vowels
+                   $base-finals)
+    (permute-parts $diphthongs
+                   $base-finals))))
